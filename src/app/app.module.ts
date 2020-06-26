d92b66c86dc27c020f65423a1febda3cc3a476ea
Ivy-N <Ivy-N>
2020-06-26 15:29:30 +0300

    includes module updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { GitusersModule } from "./components/gitusers/gitusers.module";
+import { ServicesModule } from "./services/services.module";
+import { AppRoutingModule } from "./app.routing";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, GitusersModule, ServicesModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/gitusers/gitusers.module.ts
+++ src/app/components/gitusers/gitusers.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { GitusersComponent } from "./gitusers.component";
 import { CommonModule } from "@angular/common";
-// components go into declarations, submodules into imports, services goes into providers
+
 @NgModule({
   declarations: [GitusersComponent],
   imports: [CommonModule],
